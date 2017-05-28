e3da22666140494f8fab1d4dc532c866ebe4656e
wjd345 <wjdavis1@asu.edu>
2017-05-27 22:37:05 -0700

    Initial Commit

---
+++ gitDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gitDemo
+//
+//  Created by Wesley Davis on 5/27/17.
+//  Copyright © 2017 Wesley Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let userName = "dummy user";
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
